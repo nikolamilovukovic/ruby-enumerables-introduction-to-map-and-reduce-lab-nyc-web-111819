1185b6f5b99b699d5eb8880a43eaeae9e5c8a2dd
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-11 21:56:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,15 @@ def map_to_no_change(source_array)
 end 
 
 def map_to_double(source_array)
-  
+  i = 0 
+  new_array = []
+  while i < source_array.length do
+    new_array.push(source_array[i] * 2)
+    i += 1 
+  end 
+  new_array
+end 
+
 
 
 
