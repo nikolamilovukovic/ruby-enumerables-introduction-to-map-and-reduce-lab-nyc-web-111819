65c3bf72d5e684690594d8985aa1fa4d96bc30f6
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-11 22:03:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,12 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point)
-  
+  start = starting_point
+  i = 0 
+  while i < source_array.length do
+    start += source_array[i] 
+    i += 1 
+  end 
 
 
 
