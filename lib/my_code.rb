427357105feca9ec88d0e1d45077fce6747c65c3
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-11 22:27:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-    return true if source_array[i] === true
+    return false if source_array[i] === false
     i +=1
   end
   return true
