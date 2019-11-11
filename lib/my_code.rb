e807ba8082d3882dd7e623f96f44346f33fcc546
Nikola Milovukovic <nikolamilovukovic@gmail.com>
2019-11-11 21:58:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,7 +33,7 @@ def map_to_square(source_array)
   i = 0 
   new_array = []
   while i > source_array.length do
-    new_array.push(source_array[i] ** )
+    new_array.push(source_array[i] ** source_array)
     i += 1 
   end 
   new_array
